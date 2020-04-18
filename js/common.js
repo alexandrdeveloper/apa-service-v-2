8f652ee4c5868f60ec3bab5d6864630e4e4c07c8
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-04-19 00:09:50 +0300

    Аккордеон

--- js/common.js
+++ js/common.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
 
 
-	function examples() {
-		alert('ok');
-	}
+	var acc = $('.accordeon__item > .accordeon__title');
+
+	acc.on('click', function() {
+		if ($(this).parent('.accordeon__item').hasClass('accordeon__item_active')) {
+			$(this).parent('.accordeon__item').removeClass('accordeon__item_active');
+			$(this).next('.accordeon__info').fadeOut(300);
+		} else {
+			$(this).parent('.accordeon__item').toggleClass('accordeon__item_active');
+			$(this).next('.accordeon__info').show(300);
+		}
+	});
+
+
 
 });
