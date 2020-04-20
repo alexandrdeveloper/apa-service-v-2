aa61e221bfed744925e7619bfe7d9ad110a67791
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-04-20 14:06:47 +0300

    Всплывающие окна

--- js/common.js
+++ js/common.js
@@ -30,7 +30,58 @@ $(document).ready(function() {
 		priceModal.removeClass('price-modal_active');	
 	});
 
+	$('.certificate__carousel').slick({
+		slidesToShow: 3,
+	});
 
 
+	$('.banner-image').slick({
+		slidesToShow: 1,
+		asNavFor: $('.banner-text__carousel'),
+	});
+	$('.banner-text__carousel').slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		slidesToShow: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: $('.banner-image'),
+	});
+
+	$('.certificate__link').magnificPopup({
+	  type: 'image'
+	});
+
+	
+
+
+
+	var rooms = $('.rooms'),
+		bath = $('.bathroom'),
+		roomsPlus = $('.rooms-plus'),
+		roomsMinus = $('.rooms-minus'),
+		bathPlus = $('.bathroom-plus'),
+		bathMinus = $('.bathroom-minus'),
+		roomsVal = rooms.val(),
+		bathVal = bath.val();
+
+	$('.header-categories').waypoint(function() {
+		$('.calc').toggleClass('calc_visible');
+	});
+
+	$('.contacts').waypoint(function() {
+		if($('.calc').hasClass('calc_visible')){
+			$('.calc').removeClass('calc_visible');
+		} else {
+			$('.calc').toggleClass('calc_visible');
+		}
+		
+	}, { offset: '50%' });
+
+		
+
+		
+				
+		
 
 });
